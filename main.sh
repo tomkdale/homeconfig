52b86f9d66ce3ba6d90a9fc8eda014a8bfc590b4
tdaleibm <tdale@ibm.com>
2020-04-02 20:46:35 -0400

    .

--- main.sh
+++ main.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+#########################################
+# One stop shop to get my entire environment set up on any dirstro (the goal)
+# Tom Dale
+#########################################
+
+#Get distro 
 if [ -f /etc/os-release ]; then
   . /etc/os-release
-	OS=$NAME	
-	echo "Installing on $OS"
+	DISTRO=$NAME	
+	echo "Installing on $DISTRO"
 else 
-  echo '$OS not found' 
+  echo "$DISTRO not found"
 	exit 1
 fi
 
-if [ $OS == Ubuntu ] || [ $OS == Debian ]; then 
+#install ansible then run ansible to get all package manabment 
+if [ $DISTRO == Ubuntu ] || [ $DISTRO == Debian ]; then 
   sudo apt install ansible
-elif [ $OS == "Fedora" ] || [ $OS == "RHEL" ] || [ $OS == "Centos" ]; then
+elif [ $DISTRO == "Fedora" ] || [ $DISTRO == "RHEL" ] || [ $DISTRO == "Centos" ]; then
 	sudo dnf install ansible
 fi
 
+ansible-playbook local.yml
+
 ./git-add-key.sh
+
 #Gnome Setting Changes
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 # dconf write /org/gtk/settings/file-chooser/clock-format '12h'
 
-ansible-playbook local.yml
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
