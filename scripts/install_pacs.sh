fe7b5fa754ae8623bd543c2d19557a46609be9e5
Tom <tom.k.dale@gmail.com>
2020-10-31 09:25:14 -0400

    .

--- scripts/install_pacs.sh
+++ scripts/install_pacs.sh
@@ -7,32 +7,33 @@ set -ex
 
 if grep ID /etc/os-release | grep -qE "fedora|rhel|centos|silverblue"; then
   if grep ID /etc/os-release |grep -qE "fedora"; then
-    
+    echo "Using fedora/silverblue"
   elif
+    echo "Using RHEL/CENTOS need epel packages"
     yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
   fi
 	sudo dnf install \
-	  ansible \
-	    golang \
-		git \
-		gcc \
-		make \
-		perl \
-		python3 \
-		unzip \
-		wget \
-		xclip \
-		vim \
-		curl \
-		tree \
-		ShellCheck \
-		jq \
-		w3m \
-		tmux \
-		zip \
-    moreutils
-    neovim
-    python3-neovim
+	ansible \
+	golang \
+	git \
+	gcc \
+	make \
+	perl \
+	python3 \
+	unzip \
+	wget \
+	xclip \
+	vim \
+	curl \
+	tree \
+	ShellCheck \
+	jq \
+	w3m \
+	tmux \
+	zip \
+  moreutils \
+  neovim \ 
+  python3-neovim 
 	sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
   sudo dnf install gh
 
@@ -42,27 +43,27 @@ elif grep ID /etc/os-release | grep -qE 'debian|ubuntu|raspbian'; then
 	export DEBIAN_FRONTEND DEBCONF_NONINTERACTIVE_SEEN
 	sudo apt-get update
 	sudo apt-get -yq install \
-		ansible \
-		build-essential \
-   	golang \
-		gcc \
-		net-utils \
-		git \
-		python3 \
-		python3-pip \
-		unzip \
-		wget \
-		xclip \
-		vim \
-		shellcheck \
-		curl \
-		tree \
-		jq \
-		w3m \
-		tmux \
-		zip \
-    moreutils \
-    neovim
+	ansible \
+	build-essential \
+  golang \
+	gcc \
+	net-utils \
+	git \
+	python3 \
+	python3-pip \
+	unzip \
+	wget \
+	xclip \
+	vim \
+	shellcheck \
+	curl \
+	tree \
+	jq \
+	w3m \
+	tmux \
+	zip \
+  moreutils \
+  neovim
 
 	
 
